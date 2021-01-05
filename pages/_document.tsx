8adec30cc519ceed07ef1c5a163133548a97125c
Alex Titarenko <alx.titarenko@gmail.com>
2021-01-04 17:03:15 -0800

    add custom document

---
+++ pages/_document.tsx
@@ -0,0 +1,36 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+
+class CustomDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"></meta>
+          <link rel="icon" type="image/x-icon" href="/favicon.png"></link>
+
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=%NEXT_PUBLIC_GOOGLE_ANALYTICS_ID%"></script>
+
+          <script dangerouslySetInnerHTML={{__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '%NEXT_PUBLIC_GOOGLE_ANALYTICS_ID%');
+          `}} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default CustomDocument
