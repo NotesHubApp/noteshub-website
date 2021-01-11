693b3563d1ac3877f37871ce4a24c8ac292c5a11
Alex Titarenko <alx.titarenko@gmail.com>
2021-01-10 20:23:04 -0800

    fix scroll to top button

--- components/common/ScrollToTop.tsx
+++ components/common/ScrollToTop.tsx
@@ -4,10 +4,13 @@ import { makeStyles } from '@material-ui/styles';
 const useStyles = makeStyles({
   scrollToTop: {
     position: 'fixed',
-    bottom: '0.5rem',
-    right: '0.5rem',
-    animation: '$fadeIn 700ms ease-in-out 1s both',
-    cursor: 'pointer'
+    bottom: '1rem',
+    right: '1rem',
+    animation: '$fadeIn 700ms ease-in-out both',
+    cursor: 'pointer',
+    color: 'white',
+    background: '#00518c',
+    padding: '8px 10px'
   },
   '@keyframes fadeIn': {
     from: { opacity: 0 },
@@ -40,40 +43,11 @@ export default function ScrollToTop() {
   }, []);
 
   return (
-    <div className={ classes.scrollToTop }>
+    <div>
       { visible && (
-        <div onClick={ scrollToTop }>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="38.735"
-            width="33.749"
-          >
-            <g transform="translate(-18.121 -3.364)">
-              <rect
-                ry="4.928"
-                y="3.364"
-                x="18.121"
-                height="38.735"
-                width="33.749"
-                fill="#00f"
-              />
-              <g transform="translate(-.48 2.134)">
-                <rect
-                  ry="4.928"
-                  y="1.229"
-                  x="18.601"
-                  height="38.735"
-                  width="33.749"
-                  fill="url(#b)"
-                />
-                <g fill="#ececec">
-                  <path d="M22.435 17.62l4.684 4.685 5.044-5.044v19.352h6.625V17.26l5.044 5.044 4.683-4.684-13.04-13.04z" />
-                  <path d="M22.435 17.62l4.684 4.685 5.044-5.044v19.352h6.625V17.26l5.044 5.044 4.683-4.684-13.04-13.04z" />
-                </g>
-              </g>
-            </g>
-          </svg>
-        </div>
+        <span className={ classes.scrollToTop } onClick={ scrollToTop }>
+          🡩
+        </span>
       )}
     </div>
   )
