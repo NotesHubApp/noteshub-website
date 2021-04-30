2d04cb97acc4539524d21cf1a66e5d72bae8d119
Alex Titarenko <alx.titarenko@gmail.com>
2021-04-30 10:12:56 -0700

    refactor styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,63 @@
-import Head from 'next/head'
 import React from 'react'
+import Head from 'next/head'
 import ScrollToTop from '../components/common/ScrollToTop'
 import Hero from '../components/Hero'
 import Highlights from '../components/Highlights'
-import styles from '../styles/Home.module.css'
+import { createUseStyles } from 'react-jss'
+
+const useStyles = createUseStyles({
+  container: {
+    minHeight: '100%',
+    padding: '0 0.5rem',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  main: {
+    width: '100%',
+    padding: '2rem 0',
+    flex: '1',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  footer: {
+    width: '100%',
+    height: '100px',
+    borderTop: '1px solid #eaeaea',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    '& a': {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+  },
+  copyrightText: {
+    color: '#787878'
+  }
+});
 
 export default function Home() {
+  const classes = useStyles();
+
   return (
-    <div className={styles.container}>
+    <div className={classes.container}>
       <Head>
         <title>{ `${process.env.NEXT_PUBLIC_APPNAME} - ${process.env.NEXT_PUBLIC_APPTITLE}` }</title>
       </Head>
 
-      <main className={styles.main}>
+      <main className={classes.main}>
         <Hero />
         <Highlights />
       </main>
 
-      <footer className={styles.footer}>
-        <div className={styles['copyright-text']}>
+      <footer className={classes.footer}>
+        <div className={classes.copyrightText}>
           <p>&copy; { new Date().getFullYear() } {process.env.NEXT_PUBLIC_COMPANYNAME} All rights reserved.</p>
         </div>
 
