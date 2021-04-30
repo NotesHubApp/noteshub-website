1650a56dfe74fa8dbe3640b1f0ec4903bc93f53a
Alex Titarenko <alx.titarenko@gmail.com>
2021-04-30 11:19:18 -0700

    update social links

--- next-env.d.ts
+++ next-env.d.ts
@@ -8,6 +8,9 @@ declare namespace NodeJS {
     NEXT_PUBLIC_APPTITLE: string
     NEXT_PUBLIC_APPDESC: string
     NEXT_PUBLIC_APPURL: string
+    NEXT_PUBLIC_FACEBOOK_LINK: string
+    NEXT_PUBLIC_TWITTER_LINK: string
+    NEXT_PUBLIC_INSTAGRAM_LINK: string
     NEXT_PUBLIC_COMPANYNAME: string
     NEXT_PUBLIC_GOOGLE_ANALYTICS_ID: string
   }
