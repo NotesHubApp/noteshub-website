147a9fb28a31738bdd7a7ac9d9d8aa8f393bab10
Alex Titarenko <alx.titarenko@gmail.com>
2022-04-30 21:47:46 -0700

    code refactoring

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -62,7 +62,7 @@ export default function Footer() {
       <div className={classes.copyrightText}>
         <p>&copy; { new Date().getFullYear() } {process.env.NEXT_PUBLIC_COMPANYNAME} All rights reserved.</p>
       </div>
-      <ul className={ classes.socialList }>
+      <ul className={ classes.socialList } id="contacts">
         <li>
           <a href={ process.env.NEXT_PUBLIC_FACEBOOK_LINK } title="Facebook page" target="_blank" rel="noopener noreferrer">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className={ classes.facebookIcon } viewBox="0 0 16 16">
@@ -89,7 +89,7 @@ export default function Footer() {
 
         <li>
           <a href={ `mailto:${process.env.NEXT_PUBLIC_CONTACT_EMAIL}` } title="Contact/support" target="_blank" rel="noopener noreferrer">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className={ classes.emailIcon } viewBox="0 0 24 24" data-testid="EmailIcon"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z"></path></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className={ classes.emailIcon } viewBox="0 0 24 24" data-testid="EmailIcon"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z"></path></svg>
           </a>
         </li>
       </ul>
