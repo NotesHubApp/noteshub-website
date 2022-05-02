884bf42186040c087268183af6ada360f9bf7625
Alex Titarenko <alx.titarenko@gmail.com>
2022-05-01 20:29:57 -0700

    fix build error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,57 +1,27 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
-import { createGenerateId, JssProvider, SheetsRegistry } from 'react-jss'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-class CustomDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const registry = new SheetsRegistry();
-    const generateId = createGenerateId();
-    const originalRenderPage = ctx.renderPage;
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) => (
-          <JssProvider registry={ registry } generateId={ generateId }>
-            <App {...props} />
-          </JssProvider>
-        ),
-      });
 
-    const initialProps = await Document.getInitialProps(ctx);
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" type="image/x-icon" href="/favicon.svg"></link>
 
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          <style id="server-side-styles">{registry.toString()}</style>
-        </>
-      ),
-    }
-  }
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <script async src={ `https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}` }></script>
 
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link rel="icon" type="image/x-icon" href="/favicon.svg"></link>
+        <script dangerouslySetInnerHTML={{__html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-          {/* Global site tag (gtag.js) - Google Analytics */}
-          <script async src={ `https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}` }></script>
-
-          <script dangerouslySetInnerHTML={{__html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
-          `}} />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+        `}} />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
-
-export default CustomDocument
