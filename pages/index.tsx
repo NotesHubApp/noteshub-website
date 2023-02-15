69ca30a2f29001f45337386452cfcf48b50f9c54
Alex Titarenko <alx.titarenko@gmail.com>
2023-02-14 19:59:01 -0800

    update promo banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,14 +40,14 @@ export default function Home() {
         <meta property="og:url" content={process.env.NEXT_PUBLIC_LANDING_PAGE_URL} />
         <meta property="og:title" content={process.env.NEXT_PUBLIC_APPSLOGAN} />
         <meta property="og:description" content={ process.env.NEXT_PUBLIC_APPDESC } />
-        <meta property="og:image" content={`${process.env.NEXT_PUBLIC_LANDING_PAGE_URL}/images/promo-banner.png`} />
+        <meta property="og:image" content={`${process.env.NEXT_PUBLIC_LANDING_PAGE_URL}/images/promo-banner.webp`} />
 
         {/* Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content={process.env.NEXT_PUBLIC_LANDING_PAGE_URL} />
         <meta property="twitter:title" content={process.env.NEXT_PUBLIC_APPSLOGAN} />
         <meta property="twitter:description" content={ process.env.NEXT_PUBLIC_APPDESC } />
-        <meta property="twitter:image" content={`${process.env.NEXT_PUBLIC_LANDING_PAGE_URL}/images/promo-banner.png`} />
+        <meta property="twitter:image" content={`${process.env.NEXT_PUBLIC_LANDING_PAGE_URL}/images/promo-banner.webp`} />
 
         <meta name="apple-itunes-app" content={ `app-id=${process.env.NEXT_PUBLIC_APPSTORE_APPID}` }></meta>
       </Head>
