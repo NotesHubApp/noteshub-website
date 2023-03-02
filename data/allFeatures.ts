b6932e783bd99bb601673ecc77239bbbaa1a0bbd
Alex Titarenko <alx.titarenko@gmail.com>
2023-03-01 18:32:50 -0800

    update all features list

--- data/allFeatures.ts
+++ data/allFeatures.ts
@@ -1,18 +1,23 @@
 export const features: string[] = [
+  'Works offline',
+  'No vendor lock-in',
+  'Periodic sync of data',
+  'Fully cross-platform',
+  'Adaptive UI',
+  'Seamless GitHub integration',
   'Git notebooks',
   'GitHub notebooks',
-  'iCloud Drive notebooks',
-  'File System notebooks',
-  'Local notebooks (non syncable)',
+  'iCloud Drive notebooks*',
+  'File System notebooks*',
+  'Printing notes and export to PDF',
   'Kanban boards',
-  'Dark and light themes support',
+  'Dark mode',
   'Markdown preview with scroll sync',
   'Move/copy notes across notebooks',
   'Attach/upload image file on paste from clipboard',
   'Attach/upload image file from local disk/storage',
   'Convert html content to markdown on paste from clipboard',
   'Editor toolbar with all necessary commands (bold, list, table, etc.)',
-  'Markdown syntax guidance',
   'Automatic merge conflicts resolution'
 ];
 
