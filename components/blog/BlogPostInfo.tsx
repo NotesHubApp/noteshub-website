49aac637ba6117c9543a40892b1b41621ea7c548
Alex Titarenko <alx.titarenko@gmail.com>
2023-07-22 17:02:05 -0700

    update blog post

--- components/blog/BlogPostInfo.tsx
+++ components/blog/BlogPostInfo.tsx
@@ -7,24 +7,23 @@ const useStyles = createUseStyles({
   postInfo: {
     display: 'flex',
     alignItems: 'center',
-    listStyleType: 'none',
     margin: 0,
     padding: 0,
-    gap: '3px',
+    gap: '10px',
     color: '#7f919e',
     fill: '#7f919e',
     fontSize: '0.8em',
     '& svg': {
       width: '1em'
-    },
-    '& li': {
-      display: 'flex'
     }
   },
   category: {
-    textTransform: 'uppercase',
-    marginRight: '0.6em'
+    textTransform: 'uppercase'
   },
+  postedOn: {
+    display: 'flex',
+    gap: '3px'
+  }
 })
 
 type BlogPostInfoProps = {
@@ -35,14 +34,14 @@ export function BlogPostInfo(props: BlogPostInfoProps) {
   const classes = useStyles();
 
   return (
-    <ul className={ classes.postInfo }>
-      <li className={ classes.category }>
+    <div className={ classes.postInfo }>
+      <div className={ classes.category }>
         { props.post.category.name }
-      </li>
-      <li>
+      </div>
+      <div className={ classes.postedOn }>
         <TimeIcon />
         <span>{ dateToString(new Date(props.post.postedOn)) }</span>
-      </li>
-  </ul>
+      </div>
+    </div>
   )
 }
