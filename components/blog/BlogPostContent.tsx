bdd036d573a84fedec3f00cf7163428a79a7d8ce
Alex Titarenko <alx.titarenko@gmail.com>
2023-07-23 21:46:59 -0700

    update blog post

--- components/blog/BlogPostContent.tsx
+++ components/blog/BlogPostContent.tsx
@@ -18,28 +18,14 @@ const components = {
     <a href={props.href} {...(isExternalUrl(props.href) ? { target: '_blank', rel: 'nofollow' } : {}) }>
       { props.children }
     </a>
-  ),
-  // code: (props: any) => (
-  //   <pre className={`language-${props.language}`}>
-  //     <code className={`language-${props.language}`}>{ props.value }</code>
-  //   </pre>
-  // )
+  )
 };
 
 export default function BlogPostContent(props: BlogPostContentProps) {
-  function transformInternalUri(uri: string): string {
-    if (!isExternalUrl(uri)) {
-      return uri.startsWith('/') ? `/posts${uri}` : `/posts/${uri}`;
-    }
-    return uri;
-  }
-
   return (
     <ReactMarkdown
       className='blog-post-content'
       children={props.markdownContent}
-      transformImageUri={(uri) => transformInternalUri(uri) }
-      transformLinkUri={(uri) => transformInternalUri(uri) }
       rehypePlugins={[rehypeRaw]}
       remarkPlugins={[[remarkGfm, { singleTilde: false }]]}
       components={ components }
