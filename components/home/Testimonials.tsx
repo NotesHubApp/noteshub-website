becce6223b9f596f2c86bb6d6eb50aa923406327
Alex Titarenko <alx.titarenko@gmail.com>
2023-07-31 21:13:58 -0700

    add support for full screen testimonials

--- components/home/Testimonials.tsx
+++ components/home/Testimonials.tsx
@@ -1,7 +1,10 @@
+import { useRef, useState } from 'react';
+
 import { Rating } from 'components/common/Rating';
 import { Section } from './Section';
 import { SectionTitle } from './SectionTitle';
 import { Testimonial } from 'models/Testimonial';
+import clsx from 'clsx';
 import { createUseStyles } from 'react-jss';
 import testimonials from 'data/testimonials';
 
@@ -23,7 +26,12 @@ const useStyles = createUseStyles({
   testimonialCardWrapper: {
     columnBreakInside: 'avoid',
     // This is to fix box-shadow in multi-column combination in Safari
-    padding: '5px'
+    padding: '5px',
+
+    '& article': {
+      marginBottom: '20px',
+      cursor: 'pointer'
+    }
   },
   testimonialCard: {
     textAlign: 'initial',
@@ -31,24 +39,33 @@ const useStyles = createUseStyles({
     background: 'var(--card-background)',
     borderRadius: '10px',
     padding: '20px',
-    marginBottom: '20px'
   },
   authorName: {
     marginTop: 0
   },
   testimonialContent: {
     lineHeight: 1.5,
-
+    margin: '10px 0',
+    overflow: 'auto'
+  },
+  truncatedContent: {
     // Make ellipsis on long content
     display: 'box',
     lineClamp: 10,
     boxOrient: 'vertical',
-    overflow: 'hidden',
-    margin: '10px 0'
+    overflow: 'hidden'
   },
   postedOn: {
     fontSize: '90%',
     color: 'gray'
+  },
+  fullScreenCard: {
+    maxWidth: 'var(--max-content-width)',
+    border: 'none',
+    outline: 'none',
+    padding: 0,
+    boxShadow: 'var(--box-shadow)',
+    borderRadius: '10px'
   }
 })
 
@@ -58,37 +75,65 @@ type TestimonialsProps = {
 
 export default function Testimonials({ data = testimonials }: TestimonialsProps) {
   const classes = useStyles();
+  const fullCardDialorRef = useRef<HTMLDialogElement>(null);
+  const [fullScreenCard, setFullScreenCard] = useState<Testimonial | undefined>();
+
+  const onCardClick = (testimonial: Testimonial) => {
+    setFullScreenCard(testimonial);
+    fullCardDialorRef.current?.showModal();
+  }
+
+  const onFullScreenCardClick = () => {
+    fullCardDialorRef.current?.close();
+    setFullScreenCard(undefined);
+  }
 
   return (
     <Section id="testimonials">
       <SectionTitle>Wall of Love</SectionTitle>
       <ul className={ classes.testimonialsList }>
         { data.map(testimonial => (
-          <TestimonialCard
-            key={ `${testimonial.origin}-${testimonial.author.name}` }
-            testimonial={testimonial} />)
-        )}
+          <li className={ classes.testimonialCardWrapper } onClick={ () => onCardClick(testimonial) }>
+            <TestimonialCard
+              key={ `${testimonial.origin}-${testimonial.author.name}` }
+              testimonial={testimonial} />
+          </li>
+
+        ))}
       </ul>
+
+      <dialog
+        ref={ fullCardDialorRef }
+        onClick={ onFullScreenCardClick }
+        className={ classes.fullScreenCard }
+      >
+        { fullScreenCard && (
+          <TestimonialCard testimonial={ fullScreenCard } fullScreen />
+        )}
+      </dialog>
     </Section>
   )
 }
 
-function TestimonialCard({ testimonial }: { testimonial: Testimonial }) {
+type TestimonialCardProps = {
+  testimonial: Testimonial
+  fullScreen?: boolean
+}
+
+function TestimonialCard({ testimonial, fullScreen }: TestimonialCardProps) {
   const classes = useStyles();
 
   return (
-    <li className={ classes.testimonialCardWrapper }>
-      <article className={ classes.testimonialCard }>
-        <header>
-          <h3 className={ classes.authorName }>{ testimonial.author.name }</h3>
-          { testimonial.rating && <Rating value={ testimonial.rating } /> }
-        </header>
-        <div className={ classes.testimonialContent }>{ testimonial.content }</div>
-        <footer className={ classes.postedOn }>
-          { testimonial.date.toLocaleDateString(undefined, { dateStyle: 'medium' }) }
-        </footer>
-      </article>
-    </li>
+    <article className={ classes.testimonialCard }>
+      <header>
+        <h3 className={ classes.authorName }>{ testimonial.author.name }</h3>
+        { testimonial.rating && <Rating value={ testimonial.rating } /> }
+      </header>
+      <div className={ clsx(classes.testimonialContent, !fullScreen && classes.truncatedContent) }>{ testimonial.content }</div>
+      <footer className={ classes.postedOn }>
+        { testimonial.date.toLocaleDateString(undefined, { dateStyle: 'medium' }) }
+      </footer>
+    </article>
   )
 }
 
