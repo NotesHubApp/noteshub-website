fff619d3bcc15bfa3480dba51339b440d02fbd8f
Alex Titarenko <alx.titarenko@gmail.com>
2023-10-30 10:54:07 -0700

    disable discounts

--- data/discounts.ts
+++ data/discounts.ts
@@ -1,7 +1,7 @@
 import { Discounts } from 'models/Discounts';
 
 export const discounts: Discounts = {
-  amount: 20,
+  amount: undefined,
   forAppStore: true,
   forPlayStore: true,
   forWindowsStore: true
