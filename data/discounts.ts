ca796cd2949c9867054d09673d7a90f79eef246d
Alex Titarenko <3290313+alex-titarenko@users.noreply.github.com>
2024-04-15 09:04:31 -0700

    update discounts

--- data/discounts.ts
+++ data/discounts.ts
@@ -3,6 +3,6 @@ import { Discounts } from 'models/Discounts';
 export const discounts: Discounts = {
   amount: 25,
   forAppStore: true,
-  forPlayStore: false,
-  forWindowsStore: false
+  forPlayStore: true,
+  forWindowsStore: true
 }
